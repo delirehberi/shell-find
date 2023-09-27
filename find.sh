fc2b764253c3acc6eb68d87d0feb7c293829ae32
Emre YILMAZ <z@emre.xyz>
2023-09-26 20:42:23 -0400

    show files
    Signed-off-by: Emre YILMAZ <z@emre.xyz>

--- find.sh
+++ find.sh
@@ -12,8 +12,12 @@ fi
 # Iterate through each word in the dictionary
 while read -r word; do
   # Use the find command to search for files with the current word as part of the filename
-  if find . -type f -name "*$word*" 2>/dev/null; then
-    echo "File(s) containing '$word' found."
+  found_files=$(find . -type f -name "*$word*" 2>/dev/null)
+  
+  # Check if any files were found
+  if [ -n "$found_files" ]; then
+    echo "Files containing '$word' found:"
+    echo "$found_files"
   else
     echo "No file containing '$word' found."
   fi
